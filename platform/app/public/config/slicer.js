518689cb0ceefa5facae64123c3c895e90a7f8d4
unknown <limlimg@sina.com>
2025-02-28 16:46:05 +0800

    feat(package.json, config/slicer.js): configure viewer to use custom backend

---
+++ platform/app/public/config/slicer.js
@@ -0,0 +1,54 @@
+/** @type {AppTypes.Config} */
+window.config = {
+  routerBasename: '/',
+  showStudyList: true,
+  extensions: [],
+  modes: [],
+  // below flag is for performance reasons, but it might not work for all servers
+  showWarningMessageForCrossOrigin: true,
+  showCPUFallbackMessage: true,
+  showLoadingIndicator: true,
+  experimentalStudyBrowserSort: false,
+  strictZSpacingForVolumeViewport: true,
+  studyPrefetcher: {
+    enabled: true,
+    displaySetsCount: 2,
+    maxNumPrefetchRequests: 10,
+    order: 'closest',
+  },
+  defaultDataSourceName: 'dicomweb',
+  dataSources: [
+    {
+      namespace: '@ohif/extension-default.dataSourcesModule.dicomweb',
+      sourceName: 'dicomweb',
+      configuration: {
+        friendlyName: 'DCM4CHEE',
+        name: 'DCM4CHEE',
+        wadoUriRoot: 'http://172.22.26.156:8080/dicom',
+        qidoRoot: 'http://172.22.26.156:8080/dicom',
+        wadoRoot: 'http://172.22.26.156:8080/dicom',
+        qidoSupportsIncludeField: true,
+        imageRendering: 'wadouri',
+        thumbnailRendering: 'wadouri',
+      },
+    },
+    {
+      namespace: '@ohif/extension-default.dataSourcesModule.dicomjson',
+      sourceName: 'dicomjson',
+      configuration: {
+        friendlyName: 'dicom json',
+        name: 'json',
+      },
+    },
+    {
+      namespace: '@ohif/extension-default.dataSourcesModule.dicomlocal',
+      sourceName: 'dicomlocal',
+      configuration: {
+        friendlyName: 'dicom local',
+      },
+    },
+  ],
+  httpErrorHandler: error => {
+    console.warn(`HTTP Error Handler (status: ${error.status})`, error);
+  },
+};
